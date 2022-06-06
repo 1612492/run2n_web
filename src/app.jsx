4f2916990ea37af9c1c147fc6a66b9daea70098f
Phat Vo <votanphatb2@gmail.com>
2022-06-06 10:00:36 +0700

    feat: responsive

--- src/app.jsx
+++ src/app.jsx
@@ -15,11 +15,17 @@ function App() {
   return (
     <>
       <header className="header">
-        <section className="container header__inner">
-          <a href="/">
+        <section class="container navbar">
+          <a href="#home">
             <img src={logo} alt="logo" className="logo" />
           </a>
-          <nav className="nav">
+          <input type="checkbox" id="fake-input" className="fake-input" />
+          <label for="fake-input" className="toggle-button">
+            <span className="line line--top"></span>
+            <span className="line line--middle"></span>
+            <span className="line line--bottom"></span>
+          </label>
+          <nav class="nav">
             <a href="#features" className="nav-item">
               Features
             </a>
@@ -35,9 +41,10 @@ function App() {
           </nav>
         </section>
       </header>
+
       <main className="main">
         <section className="container">
-          <section className="intro">
+          <section id="home" className="intro">
             <h3 className="heading">SOME HEADING HERE</h3>
 
             <p className="sub-heading">
@@ -51,38 +58,38 @@ function App() {
           <section id="features" className="features">
             <h2 className="title">Features</h2>
             <article className="feature">
-              <section className="feature__side">
+              <section className="feature__content-container">
                 <h3 className="feature__title">RUN TO EARN</h3>
                 <p className="feature__detail">
                   Dorem ipsum dolor sitamet, consectetur adipiscing elit, sed do eiusm tempor
                   incididunt ulabore et dolore magna aliqua.
                 </p>
               </section>
-              <section className="feature__side feature__side--flex">
+              <section className="feature__image-container">
                 <img src={runningImage} alt="Run to earn" className="feature__image" />
               </section>
             </article>
-            <article className="feature">
-              <section className="feature__side feature__side--flex">
-                <img src={marketplaceImage} alt="NFT Marketplace" className="feature__image" />
-              </section>
-              <section className="feature__side">
+            <article className="feature feature--reverse">
+              <section className="feature__content-container">
                 <h3 className="feature__title">NFT MARKETPLACE</h3>
                 <p className="feature__detail">
                   Dorem ipsum dolor sitamet, consectetur adipiscing elit, sed do eiusm tempor
                   incididunt ulabore et dolore magna aliqua.
                 </p>
               </section>
+              <section className="feature__image-container">
+                <img src={marketplaceImage} alt="NFT Marketplace" className="feature__image" />
+              </section>
             </article>
             <article className="feature">
-              <section className="feature__side">
+              <section className="feature__content-container">
                 <h3 className="feature__title">DECENTRALIZED WALLET</h3>
                 <p className="feature__detail">
                   Dorem ipsum dolor sitamet, consectetur adipiscing elit, sed do eiusm tempor
                   incididunt ulabore et dolore magna aliqua.
                 </p>
               </section>
-              <section className="feature__side feature__side--flex">
+              <section className="feature__image-container">
                 <img
                   src={walletImage}
                   alt="Decentralized wallet"
@@ -90,20 +97,20 @@ function App() {
                 />
               </section>
             </article>
-            <article className="feature">
-              <section className="feature__side feature__side--flex">
-                <img
-                  src={activityImage}
-                  alt="Tracking your training"
-                  className="feature__small-image"
-                />
-              </section>
-              <section className="feature__side">
+            <article className="feature feature--reverse">
+              <section className="feature__content-container">
                 <h3 className="feature__title">TRACKING YOUR TRAINING</h3>
                 <p className="feature__detail">
                   Dorem ipsum dolor sitamet, consectetur adipiscing elit, sed do eiusm tempor
                   incididunt ulabore et dolore magna aliqua.
                 </p>
+              </section>
+              <section className="feature__image-container">
+                <img
+                  src={activityImage}
+                  alt="Tracking your training"
+                  className="feature__small-image"
+                />
               </section>
             </article>
           </section>
@@ -134,28 +141,28 @@ function App() {
             <section class="bullets">
               <article class="bullet">
                 <img src={checkedImage} className="bullet__icon" />
-                <article>
+                <article className="bullet__content">
                   <h5 className="roadmap__title">LAUNCH</h5>
                   <p>Moonstar is born and a new journey start.</p>
                 </article>
               </article>
               <article class="bullet">
                 <img src={checkedImage} className="bullet__icon" />
-                <article>
+                <article className="bullet__content">
                   <h5 className="roadmap__title">INITIAL EXPOSURE</h5>
                   <p>Advertising, influencers marketing push</p>
                 </article>
               </article>
               <article class="bullet">
                 <img src={checkedImage} className="bullet__icon" />
-                <article>
+                <article className="bullet__content">
                   <h5 className="roadmap__title">CORE TEAM BUILDING</h5>
                   <p>We are at 50% at the moment and looking for some new members</p>
                 </article>
               </article>
               <article class="bullet">
                 <img src={notCheckedImage} className="bullet__icon" />
-                <article>
+                <article className="bullet__content">
                   <h5 className="roadmap__title">COMMUNITY 65%</h5>
                   <p>
                     Fudders and people that came for a quick buck are starting to leave out of
@@ -165,7 +172,7 @@ function App() {
               </article>
               <article class="bullet">
                 <img src={notCheckedImage} className="bullet__icon" />
-                <article>
+                <article className="bullet__content">
                   <h5 className="roadmap__title">PRODUCT DEVELOPMENT 40%</h5>
                   <p>
                     We are working on a new website, talking with a dev to bring a dApp and NFT shop
@@ -174,14 +181,14 @@ function App() {
               </article>
               <article class="bullet">
                 <img src={notCheckedImage} className="bullet__icon" />
-                <article>
+                <article className="bullet__content">
                   <h5 className="roadmap__title">FURTHER EXPOSURE</h5>
                   <p>We are preparing soon for a second marketing push</p>
                 </article>
               </article>
               <article class="bullet">
                 <img src={notCheckedImage} className="bullet__icon" />
-                <article>
+                <article className="bullet__content">
                   <h5 className="roadmap__title">PARTNERSHIPS 30%</h5>
                   <p>
                     We are in the talks with 2 big projects to collaborate with, this will boost us
@@ -191,7 +198,7 @@ function App() {
               </article>
               <article class="bullet">
                 <img src={notCheckedImage} className="bullet__icon" />
-                <article>
+                <article className="bullet__content">
                   <h5 className="roadmap__title">TO THE STARS</h5>
                   <p>Much more to come!</p>
                 </article>
@@ -210,6 +217,9 @@ function App() {
                 Your message
               </label>
               <textarea id="message" rows={10} className="form__input" />
+              <button type="button" className="submit-button">
+                Submit
+              </button>
             </form>
           </section>
         </section>
