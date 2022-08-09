bba49b66559abe8a725414dc32e5084a61575149
Phat Vo <votanphatb2@gmail.com>
2022-08-09 16:26:43 +0700

    chore: add community links

---
+++ frontend/src/components/community.jsx
@@ -0,0 +1,21 @@
+function Community() {
+  return (
+    <section>
+      <h2>Join our community</h2>
+      <p>Join one of the most vibrant communities, and shape the future of Run2n.</p>
+      <section>
+        <a>
+          <img src="discord" alt="discord" />
+        </a>
+        <a>
+          <img src="telegram" alt="telegram" />
+        </a>
+        <a>
+          <img src="twitter" alt="twitter" />
+        </a>
+      </section>
+    </section>
+  );
+}
+
+export default Community;
--- frontend/src/components/intro.jsx
+++ frontend/src/components/intro.jsx
@@ -1,3 +1,7 @@
+import discordImage from '../images/discord.png';
+import telegramImage from '../images/telegram.png';
+import twitterImage from '../images/twitter.png';
+
 function Intro() {
   return (
     <section id="home" className="intro">
@@ -7,15 +11,35 @@ function Intro() {
         Run2n is a Web3 fitness and lifestyle mobile app with inbuilt NFT gaming and Social-fi
         elements where you can earn rewards in Crypto/NFTs just by movement like walking or running.
       </p>
-
-      <a
-        rel="noreferer"
-        target="_blank"
-        href="https://t.me/+rdILeuiQjg9lNDc1"
-        className="intro__button"
-      >
-        JOIN
-      </a>
+      <section className="community">
+        <p className="community__title">Join our community</p>
+        <section className="community__links">
+          <a
+            rel="noreferer"
+            target="_blank"
+            href="https://discord.gg/P5MKdpeubT"
+            className="community__link"
+          >
+            <img src={discordImage} alt="discord" className="community__image" />
+          </a>
+          <a
+            rel="noreferer"
+            target="_blank"
+            href="https://t.me/run2nio"
+            className="community__link"
+          >
+            <img src={telegramImage} alt="telegram" className="community__image" />
+          </a>
+          <a
+            rel="noreferer"
+            target="_blank"
+            href="https://twitter.com/run2ndotio"
+            className="community__link"
+          >
+            <img src={twitterImage} alt="twitter" className="community__image" />
+          </a>
+        </section>
+      </section>
     </section>
   );
 }
