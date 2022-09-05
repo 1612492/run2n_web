be75cc6dad29d420b1e9253a24c23683c43e5291
Phat Vo <votanphatb2@gmail.com>
2022-09-05 08:17:44 +0700

    fix: duplicate footer

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,4 +1,3 @@
-import Header from '../components/header';
 import Intro from '../components/intro';
 import Video from '../components/video';
 import Features from '../components/features';
@@ -6,24 +5,19 @@ import Marketplace from '../components/marketplace';
 import Roadmap from '../components/roadmap';
 import Testnet from '../components/testnet';
 import Contact from '../components/contact';
-import Footer from '../components/footer';
 import Main from '../components/main';
 
 function Home() {
   return (
-    <>
-      <Header />
-      <Main>
-        <Intro />
-        <Video />
-        <Features />
-        <Marketplace />
-        <Roadmap />
-        <Testnet />
-        <Contact />
-      </Main>
-      <Footer />
-    </>
+    <Main>
+      <Intro />
+      <Video />
+      <Features />
+      <Marketplace />
+      <Roadmap />
+      <Testnet />
+      <Contact />
+    </Main>
   );
 }
 
